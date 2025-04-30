e0b67c5068a97ef930415e289a1c2fe3116e0434
Daniel-LSP <gcinovation12@gmail.com>
2025-04-30 18:40:36 -0500

    main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-
+from livereload import Server
 app=Flask(__name__)
 
 @app.route("/")
@@ -7,4 +7,8 @@ def home():
 
     return "PAGINA DE INICIO"
 
+if __name__ == "__main__":
 
+    server = Server(app.wsgi_app)
+
+    server.serve()
